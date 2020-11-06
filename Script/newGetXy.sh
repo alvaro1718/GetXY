cd9a8527c175de8244026c7c87914a0dcaa68a46
alvaro1718 <36928711+alvaro1718@users.noreply.github.com>
2020-11-06 15:01:51 +0000

    info adicional

--- Script/newGetXy.sh
+++ Script/newGetXy.sh
@@ -9,6 +9,23 @@ FS=" "
 {
 
 if ( $7==53 && $8>0 ) X=$8; if( $7==54 && $8>0 ) Y=$8;
+
+if ( ( $7==53 && $8>0 ) || ( $7==54 && $8>0 ) ) {
+
+ if( X < 100 && Y < 100)
+ { print "\033[K(canto superior esquerdo)";exit; }
+
+ if( X > 800 && Y < 100)
+ {  print "\033[K(canto superior direito teste)"; }
+
+ if( X < 100 && Y > 400)
+ { print "\033[K(canto inferior esquerdo teste teste)"; }
+
+ if( X > 800 && Y > 400)
+ { print "\033[K(canto inferior direito teste teste)"; }
+
+};
+
 print "\033[4;70H--------------------";
 print "\033[4;63H-------";
 print "\033[4;70H(X:"X", Y:"Y")"
